ebc50d3537652593b1d17fcb38c57c46d5ce341e
Arcadi Garcia <arcadigarciarius21@gmail.com>
2017-06-12 09:48:44 +0200

    Ahora sí
    YASSSSSS

--- main.c
+++ main.c
@@ -28,58 +28,6 @@ int main(){
 	softPwmCreate(PINR, INIVAL, RANGO);
 	softPwmCreate(PINL, INIVAL, RANGO);
 	int izq=0, dch=0; /* Marchas PWM */
-    
-	//printf("Pulsa ENTER para ser guay.\n");
-	///getchar();
-    
-    //Avanzar 2m y girar 90 cm
-    /*advance(200);
-    printf("¡Aparcao!\n");
-    delay(250);
-    printf("¡Voy a girar...!\n");
-    gira(-90);
-    printf("¡Ya he girao!\n");*/
-    
-    //Hacer toda la maniobra de detectar obstáculos
-	/*gofw(1);
-	while(dista[0] == -1 && dista[1] == -1){}
-	stop(); //para al detectar algo
-    delay(1000);
-    if(dista[0] == -1 && dista[1] != -1){
-        who = 1;
-    } else {
-        who = 0;
-    }
-    gofw(1);
-    while(dista[who] > 20){}
-    stop();
-	delay(1000);
-	gira(90);
-    delay(100);
-    advance(40);
-    delay(100);
-    gira(-90);
-    delay(100);
-    advance(50);*/
-    
-    /* int i,len,orit;
-    int** path;
-    
-    printf("Dime la coordenada x y ori inicial:\n");
-    scanf("%d %d %d", &mypos[0], &mypos[1], &ori);
-    printf("Vale, parto en (%d, %d) con orientación %dº.\n",mypos[0],mypos[1],ori);
-    
-    printf("\n Dime por cuántos puntos quieres que pase:\n");
-    scanf("%d", &len);
-    
-    path = malloc(len*sizeof(int*));
-    printf("Ve especificándome uno por uno los puntos por los que quieres que pase.\n");
-    for(i=0; i<len; i++){
-        path[i] = malloc(2*sizeof(int));
-        scanf("%d %d",&path[i][0],&path[i][1]);
-    }
-    
-    follow(path,len);*/
 
     int n;
     int po[2]={0,0};
@@ -113,12 +61,13 @@ int main(){
         // int y = esquivarSetup(); <-- esto pondría en marcha el monitor de obstáculos
         followGraph(len);
 
-        if(!done){ //done se pone a 1 si hemos interrumpido la ruta por un obstáculo
+        /* if(!done){ //done se pone a 1 si hemos interrumpido la ruta por un obstáculo
             break;
         }
         resetMap(); //quitamos los '3' que pintamos para ver la ruta
         po[0] = mypos[0]/10;
-        po[1] = mypos[1]/10;
+        po[1] = mypos[1]/10; */ 
+        break;
     }
     return 0;
 }
